be6cfa84c4b28d3d9c889c45d1e84d7bb6d08064
AdamRse <bakaarion3@gmail.com>
2024-01-03 19:53:51 +0100

    merge

--- index.php
+++ index.php
@@ -9,7 +9,13 @@ include './classes/Bdd-Exo-2.class.php';
     <title>Exo PDO</title>
 </head>
 <body>
-    
+<?php
+$s1="S1";
+$s2="S2";
+$tab=array("S3", "S4");
+
+echo "a".(2+4);
+?>
     <ul>
         <li><a href="./ajout-patient.php">Ajouter un patient</a></li>
         <li><a href="./liste-patients.php">Afficher les patients</a></li>
