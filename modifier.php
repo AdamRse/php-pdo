e4a13990d0b889dd6eef780a2dd026982a75d0e8
Garage404 <bakaarion3@gmail.com>
2024-01-04 11:07:46 +0100

    fini

--- modifier.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!empty($_GET['lastname']) && !empty($_GET['firstname'])){
-    require "./PageRequetes/modifier-bdd.php";
-}
-else{
-    require "./PageRequetes/modifier-formulaire.php";
-}
